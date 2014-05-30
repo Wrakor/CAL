19d63ca422efed82d830dab0bf3b9cfcbad8df81
Wrakor <nunocruz59@hotmail.com>
2014-05-30 16:45:18 +0100

    melhoramentos

--- Projecto 2/Mastermind/mastermind.cpp
+++ Projecto 2/Mastermind/mastermind.cpp
@@ -49,7 +49,7 @@ vector<Theme> readDictionary() {
 	return themes;
 }
 
-bool bot(string word, vector<Theme> dictionary, int index, int player) {
+bool play(string word, vector<Theme> dictionary, int index, int player) {
 
 	bool win = false;
 	string tryWord, check[word.size()]; //tryWord - tentativa para adivinhar a palavra // check - guarda os resultado da tentativa com B, W ou _
@@ -81,6 +81,11 @@ bool bot(string word, vector<Theme> dictionary, int index, int player) {
 			cout << "(" << word.size() << " letras)" << endl;
 
 			cin >> tryWord;
+
+			while (word.size() != tryWord.size()){ //se o tamanho for diferente nao aceita
+				cout << "Numero de letras nao coincide! Tente outra vez:";
+				cin >> tryWord;
+			}
 		}
 		else if (player == 2) { //computador
 			//primeira tentativa é a primeira plavra que encontrar com o mesmo numero de letras
@@ -157,77 +162,78 @@ bool bot(string word, vector<Theme> dictionary, int index, int player) {
 		}
 		existingLetters.clear();
 
-		if(word.size() == tryWord.size()){//se o tamanho for diferente nao aceita
-			//actualiza o counter com a quantidade de cada letra
-			for(size_t i = 0; i < word.size(); i++){
-				if(!letterCounter.empty()){
-					if(letterCounter.find(word[i]) == letterCounter.end()){ //a letra ainda nao foi inicializada no counter
-						letterCounter[word[i]]=1;
-					}else{ //a letra ja existe
-						letterCounter[word[i]]+=1;
-					}
-				}else{//counter vazio
+
+		//actualiza o counter com a quantidade de cada letra
+		for(size_t i = 0; i < word.size(); i++){
+			if(!letterCounter.empty()){
+				if(letterCounter.find(word[i]) == letterCounter.end()){ //a letra ainda nao foi inicializada no counter
 					letterCounter[word[i]]=1;
+				}else{ //a letra ja existe
+					letterCounter[word[i]]+=1;
 				}
+			}else{//counter vazio
+				letterCounter[word[i]]=1;
 			}
+		}
 
-			//procura letras ja na posicao certa; actualiza o check e o counter e incrementa o match
+		//procura letras ja na posicao certa; actualiza o check e o counter e incrementa o match
+		for(size_t i = 0; i < word.size(); i++){
+			if(word[i]==tryWord[i]){
+				check[i]="B";
+				letterCounter[word[i]]--;
+				matchingLetters++;
+			}
+		}
+
+		if(matchingLetters < word.size()){
+			//verifica o resto das letras (fora da posiçao e nao existentes)
 			for(size_t i = 0; i < word.size(); i++){
-				if(word[i]==tryWord[i]){
-					check[i]="B";
-					letterCounter[word[i]]--;
-					matchingLetters++;
-				}
-			}
+				for( size_t j = 0; j < tryWord.size(); j++){
+					if(i != j && word[i] == tryWord[j] && letterCounter[word[i]] > 0 && check[j] != "B"){//só conta como W se nao exceder o nr de letras da palavra
+						check[j] = "W";
+						letterCounter[word[i]]=letterCounter[word[i]]-1;
+					}else if(check[j]==" "){//no caso de exceder o nr de letras, por exemplo, 2º a em canas é tratado como _ apesar de pertencer à palavra
+						check[j]="_";
 
-			if(matchingLetters < word.size()){
-				//verifica o resto das letras (fora da posiçao e nao existentes)
-				for(size_t i = 0; i < word.size(); i++){
-					for( size_t j = 0; j < tryWord.size(); j++){
-						if(i != j && word[i] == tryWord[j] && letterCounter[word[i]] > 0 && check[j] != "B"){//só conta como W se nao exceder o nr de letras da palavra
-							check[j] = "W";
-							letterCounter[word[i]]=letterCounter[word[i]]-1;
-						}else if(check[j]==" "){//no caso de exceder o nr de letras, por exemplo, 2º a em canas é tratado como _ apesar de pertencer à palavra
-							check[j]="_";
+						//Usado para o computador:
+						if (player == 2) {
+							//verifica se existem mais letras iguais (para o caso de ter sido encontrada antes uma letra igual que foi classificada com W)
+							bool exists = false;
 
-							//Usado para o computador:
-							if (player == 2) {
-								//verifica se existem mais letras iguais (para o caso de ter sido encontrada antes uma letra igual que foi classificada com W)
-								bool exists = false;
+							for (size_t k = 0; k < tryWord.size(); k++)
+								if (tryWord[j] == tryWord[k])
+									exists = true;
 
-								for (size_t k = 0; k < tryWord.size(); k++)
-									if (tryWord[j] == tryWord[k])
-										exists = true;
-
-								//senao existirem, entao a palavra a acertar nao tem esta letra, sendo adicionada ao respectivo vetor
-								if (!exists)
-									nonExistingLetters += check[j];
-							}
+							//senao existirem, entao a palavra a acertar nao tem esta letra, sendo adicionada ao respectivo vetor
+							if (!exists)
+								nonExistingLetters += check[j];
 						}
 					}
 				}
 			}
+		}
 
-			//print da jogada
-			for(size_t i = 0; i < tryWord.size(); i++){
-				cout << check[i] << " ";
-			}
-			cout<< endl;
-			for(size_t i = 0; i < word.size(); i++){
-				cout << check[i] << " ";
-			}
-			cout << endl << endl;
+		//print da jogada
+		for(size_t i = 0; i < tryWord.size(); i++){
+			cout << tryWord[i] << " ";
+		}
+		cout<< endl;
+		for(size_t i = 0; i < word.size(); i++){
+			cout << check[i] << " ";
+		}
+		cout << endl << endl;
 
-			if (player == 2) {
-				//ve as letras marcadas com W e B e modifica o check e actualiza o existingLetters para ser usado pelo computador na proxima iteracao
-				for (size_t i = 0; i < word.size(); i++) {
-					if (check[i] == "B")
-						check[i] = tryWord[i];
-					else if (check[i] == "W")
-						existingLetters += tryWord[i];
-				}
+		if (player == 2) {
+			//ve as letras marcadas com W e B e modifica o check e actualiza o existingLetters para ser usado pelo computador na proxima iteracao
+			for (size_t i = 0; i < word.size(); i++) {
+				if (check[i] == "B")
+					check[i] = tryWord[i];
+				else if (check[i] == "W")
+					existingLetters += tryWord[i];
 			}
 		}
+
+
 		//fim da jogada
 		play++;
 
@@ -259,7 +265,7 @@ int main()
 
 	cin >> player;
 
-	while (!(playerNumber = atoi(player.c_str()))) {
+	while (!(playerNumber = atoi(player.c_str())) || playerNumber < 1 || playerNumber > 2) {
 		cout << "Input invalido! Escolha o jogador:";
 		cin >> player;
 	}
@@ -271,7 +277,7 @@ int main()
 
 	cin >> gameMode;
 
-	while (!(mode = atoi(gameMode.c_str()))) {
+	while (!(mode = atoi(gameMode.c_str())) || mode > 3 || mode < 1) {
 		cout << "Input invalido! Escolha o modo de jogo:";
 		cin >> gameMode;
 	}
@@ -308,15 +314,18 @@ int main()
 
 		wordIndex = rand() % dictionary[themeIndex].getWords().size();
 		cout << "Tema: " << dictionary[themeIndex].getName() << endl;
+		cout << "Legenda:" << endl;
+		cout << "	B - Black" << endl;
+		cout << "	W - White" << endl;
 		palavra = dictionary[themeIndex].getWords()[wordIndex];
 
 		break;
 	}
 
 	if (playerNumber == 1)
-		bot(palavra, dictionary, themeIndex, 1);
+		play(palavra, dictionary, themeIndex, 1);
 	else
-		bot(palavra, dictionary, themeIndex, 2);
+		play(palavra, dictionary, themeIndex, 2);
 
 	return 0;
 }
